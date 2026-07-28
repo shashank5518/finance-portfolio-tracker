bfa99aba1b1d704676cb360ec1cc1947ddc8c3d4
Shashank <shashankg5518@gmail.com>
2026-07-28 19:46:24 +0530

    alembic instantiated

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .bank_account import BankAccount
+from .bank_category import BankCategory
+from .bank_transaction import BankTransaction
+from .demat_account import DematAccount
+from .demat_holding import DematHolding
+from .demat_transaction import DematTransaction
--- models/user.py
+++ models/user.py
@@ -17,6 +17,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    phone: Mapped[str] = mapped_column(String(10), unique=True, nullable = False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
